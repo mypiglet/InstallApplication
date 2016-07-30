7af30fcb5051844db2d32b226bc0c2ad940587c3
Ohralathe <mypiglet@vxhlovediscovery@yahoo.com.vn>
2016-07-30 22:19:26 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# InstallApps
---
+++ main.sh
@@ -0,0 +1,10 @@
+##!bin/bash
+#sudo apt-get install software-properties-common -y
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt-get update -y
+#sudo apt-get install ansible -y
+
+#Run ansible playbook
+ansible-playbook -i inventory/local/ubuntu-local.yml install/install-all.yml
+
+
